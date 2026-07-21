bb724c2529bcd2c19e41805710117869835519ac
adust09 <shoukitsuda@icloud.com>
2026-07-21 12:08:33 +0900

    refactor: standardize lazy ByteString alias to BL
    The Core modules aliased Data.ByteString.Lazy as LBS while the rest of
    the codebase (7 modules) uses BL. Unify on the majority BL convention.

--- src/Network/LibP2P/Core/Binary.hs
+++ src/Network/LibP2P/Core/Binary.hs
@@ -12,21 +12,21 @@ module Network.LibP2P.Core.Binary
 import Data.Binary.Get (getWord16be, getWord32be, runGet)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Builder as Builder
-import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy as BL
 import Data.Word (Word16, Word32)
 
 -- | Encode a Word16 as 2-byte big-endian ByteString.
 word16BE :: Word16 -> ByteString
-word16BE = LBS.toStrict . Builder.toLazyByteString . Builder.word16BE
+word16BE = BL.toStrict . Builder.toLazyByteString . Builder.word16BE
 
 -- | Encode a Word32 as 4-byte big-endian ByteString.
 word32BE :: Word32 -> ByteString
-word32BE = LBS.toStrict . Builder.toLazyByteString . Builder.word32BE
+word32BE = BL.toStrict . Builder.toLazyByteString . Builder.word32BE
 
 -- | Read a big-endian Word16 from a ByteString (must be >= 2 bytes).
 readWord16BE :: ByteString -> Word16
-readWord16BE = runGet getWord16be . LBS.fromStrict
+readWord16BE = runGet getWord16be . BL.fromStrict
 
 -- | Read a big-endian Word32 from a ByteString (must be >= 4 bytes).
 readWord32BE :: ByteString -> Word32
-readWord32BE = runGet getWord32be . LBS.fromStrict
+readWord32BE = runGet getWord32be . BL.fromStrict
